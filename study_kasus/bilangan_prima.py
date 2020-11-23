57b20728818d5cb6743292baa026926f6e1baf0b
danil syah <danilsyaharihardjo@gmail.com>
2020-11-23 14:39:10 +0700

    update bilanga_prima

--- study_kasus/bilangan_prima.py
+++ study_kasus/bilangan_prima.py
@@ -1,14 +1,14 @@
 bilangan = int(input("Masukan bilangan = "))
 list_bilangan = [i for i in range(2, bilangan+1)]
 print(list_bilangan)
-print("-"* 50)
+print("-" * 50)
 
 bilangan_prima = []
 
 for a in list_bilangan:
     mod = 1
     for b in range(2, a):
-        if(a % b == 0):
+        if a % b == 0:
             mod = 0
     if mod == 1:
         bilangan_prima.append(a)
